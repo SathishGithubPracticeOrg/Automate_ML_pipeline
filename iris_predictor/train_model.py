e3417640339d99e1001a36f71d0b6cfc2e952d4e
Sathish K S <sathee.911@gmail.com>
2022-10-11 19:18:37 +0530

    Changed nestimators to 10 from 100

--- iris_predictor/train_model.py
+++ iris_predictor/train_model.py
@@ -10,7 +10,7 @@ Config.MODELS_PATH.mkdir(parents = True, exist_ok = True)
 X_train = pd.read_csv(str(Config.FEATURES_PATH/"train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH/"train_labels.csv"))
 
-model =RandomForestClassifier(n_estimators=100,n_jobs=3)
+model =RandomForestClassifier(n_estimators=10,n_jobs=3)
 model = model.fit(X_train, y_train.to_numpy().ravel())
 
 pickle.dump(model, open(str(Config.MODELS_PATH/"model.pickle"), "wb"))
